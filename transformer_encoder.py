9c130587a631f141c70a91993fd0c751209a88de
GuilhermeBenigno <guilhermegomesbenigno@gmail.com>
2026-03-10 18:47:33 -0300

    Add vocabulary dictionary for transformer encoder

---
+++ transformer_encoder.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+np.random.seed(42)
+
+vocab_dict = {
+    "o":        0,
+    "banco":    1,
+    "bloqueou": 2,
+    "cartao":   3,
+    "de":       4,
+    "credito":  5,
+    "meu":      6,
+    "sem":      7,
+    "motivo":   8,
+}
