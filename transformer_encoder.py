e18a08fba39b4d1781e3c6fb17d25639de974a88
GuilhermeBenigno <guilhermegomesbenigno@gmail.com>
2026-03-10 19:01:31 -0300

    Implement softmax and scaled dot product attention
    Added softmax function and scaled dot product attention implementation.

--- transformer_encoder.py
+++ transformer_encoder.py
@@ -39,3 +39,24 @@ print(f"IDs da frase     : {input_ids}")
 X = embedding_table[input_ids]
 X = X[np.newaxis, :, :]
 
+print(f"Shape do tensor de entrada X : {X.shape}  →  (Batch, Tokens, d_model)")
+print()
+
+def softmax(x):
+    x_shifted = x - np.max(x, axis=-1, keepdims=True)
+    exp_x     = np.exp(x_shifted)
+    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+
+def scaled_dot_product_attention(X, WQ, WK, WV):
+    d_k = WK.shape[1]
+    
+    Q = X @ WQ
+    K = X @ WK   
+    V = X @ WV
+    scores = Q @ K.transpose(0, 2, 1) / np.sqrt(d_k)
+    
+    attn_weights = softmax(scores)
+    
+    output = attn_weights @ V
+    
+
